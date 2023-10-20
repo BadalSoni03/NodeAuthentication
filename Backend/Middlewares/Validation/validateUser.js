9f7a3269f98fa06cc096037509625c8dbea0c1b2
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-20 23:36:12 +0530

    Update validateUser.js

--- Backend/Middlewares/Validation/validateUser.js
+++ Backend/Middlewares/Validation/validateUser.js
@@ -12,6 +12,7 @@ const validateUserRegister = [
 	.withMessage('Name must be between 3 to 20 characters'),
 	
 	check('email')
+	.trim()
 	.normalizeEmail({gmail_remove_dots: false})
 	.isEmail()
 	.withMessage('Enter a valid email id'),
